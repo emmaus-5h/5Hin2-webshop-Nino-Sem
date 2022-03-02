1525c72f214153701cb41dc9626573d484c3cef7
Sem-5HD <74540104+Sem-4HA@users.noreply.github.com>
2022-03-02 17:28:45 +0000

    vanalles

--- db/create.sql
+++ db/create.sql
@@ -9,6 +9,7 @@ CREATE TABLE products (
   description TEXT,
   price NUMERIC(10, 2),
   adviesprijs NUMERIC (10, 2),
+  gewicht VARCHAR (255),
   voorraad_id INTEGER
 );
 
@@ -40,21 +41,21 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Piano Yamaha P-127', 'Yamaha P-125 is de opvolger van de succesvolle Yamaha P-115, deze piano komt met nieuwe functies zoals drumtracks, vleugelklanken en nog meer.', '100435', 849.99, 950);
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Ibanez Guitar', 'Ibanez houten elektrische Guitar', '077030122-3', 150, 230);
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Fender Guitar', 'Ibanez Fender Guitar, elektrisch', '445924201-X', 350, 400);
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Devine PRO Koptelefoon', 'Bluetooth headphone', '693155505-7', 40, 60);
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Yamaha Stage shellset Pure White Drum', 'Volledig drumstel', '686928463-6', 780, 900);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Piano Yamaha P-127', 'Yamaha P-125 is de opvolger van de succesvolle Yamaha P-115, deze piano komt met nieuwe functies zoals drumtracks, vleugelklanken en nog meer.', '100435', 849.99, 950, '16 kg');
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Ibanez Guitar', 'Ibanez houten elektrische Guitar', '077030122-3', 150, 230, '3.9 kg');
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Fender Guitar', 'Ibanez Fender Guitar, elektrisch', '445924201-X', 350, 400, '15 kg');
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Devine PRO Koptelefoon', 'Bluetooth headphone', '693155505-7', 40, 60, '1.1');
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Yamaha Stage shellset Pure White Drum', 'Volledig drumstel', '686928463-6', 780, 900, '50 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('Fazley elektrische basgitaar rood', 'Basgitaar Rood', '492662523-7', 80, 110);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Fazley elektrische basgitaar rood', 'Basgitaar Rood', '492662523-7', 80, 110, '5 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('test 1', 'test 1', '492662523-7', 80, 110);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('LaPaz C30BK klassieke gitaar zwart + statief + accessoires', 'LaPaz biedt met de C30 een uitstekende optie voor iedereen die kennis wil maken met de klassieke gitaar. Bovendien maak je met deze vriendelijk geprijsde nylon-snarige gitaar direct een goede start. Dit model is namelijk opgebouwd uit een sparrenhouten bovenblad met zij- en achterkanten van lindehout (basswood). Hierdoor kun je rekenen op een gebalanceerde klank met voldoende helderheid en een warme basisklank. Er is gekozen voor een zijdeglans afwerking voor zowel de body en de hals. Dit ziet er niet alleen bijzonder chic uit, het draagt ook bij aan een hoger speelcomfort. Deze versie in 4/4-formaat is geschikt voor kinderen en volwassenen vanaf 12 jaar oud. Inclusief statief, voetenbankje, clip-on tuner en een capo.', '492662523-7', 120, 130, '3.9 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('test 2', 'test 2', '492662523-7', 80, 110);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Medeli GRAND510 Black digitale vleugel', 'De Medeli GRAND510 is een baby grand piano, wat betekent dat deze digitale piano met zijn vleugelbehuizing zeer geschikt is voor thuisgebruik. Dit K8 triple sensor keyboard geeft je bovendien de klank en feel van een grand piano. Zo komt het getrouwe speelgevoel door de 88 toetsen met hammer action. Door de sensoren wordt de release op 3 plekken vastgelegd, wat ervoor zorgt dat muziekstukken in verschillende tempos correct worden doorgegeven. Gecombineerd met de polyfonie van 256 kun je er vanuit gaan dat je alles wat op de toetsen losgaat ook daadwerkelijk terugkrijgt.', '492662523-7', 80, 110, '115 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('test 3', 'test 3', '492662523-7', 80, 110);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('test 3', 'test 3', '492662523-7', 80, 110, '10 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs) values ('test 4', 'test 4', '492662523-7', 80, 110);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('test 4', 'test 4', '492662523-7', 80, 110, '10 kg');
 
 insert into winkels (winkelcode,name) values ('ROT', 'Rotterdam');
 insert into winkels (winkelcode,name) values ('UTR','Utrecht');
