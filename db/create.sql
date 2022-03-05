6372cdaa39ce58f9a1b5fa0a0ddbdb2bb6550c13
Sem-5HD <74540104+Sem-4HA@users.noreply.github.com>
2022-03-05 17:45:32 +0000

    Winkels

--- db/create.sql
+++ db/create.sql
@@ -55,10 +55,14 @@ insert into productgroepen (name) values ('Koptelefoon');
 insert into productgroepen (name) values ('Drumstel');
 
   
-insert into products (name, description, artikelcode, price, adviesprijs, gewicht, productgroep_id) values ('Piano Yamaha P-127', 'Yamaha P-125 is de opvolger van de succesvolle Yamaha P-115, deze piano komt met nieuwe functies zoals drumtracks, vleugelklanken en nog meer.', '100435', 849.99, 950, '16 kg',1);
-insert into products (name, description, artikelcode, price, adviesprijs, gewicht, productgroep_id) values ('Ibanez Guitar', 'Ibanez houten elektrische Guitar', '077030122-3', 150, 230, '3.9 kg',1);
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht, productgroep_id) values ('Yamaha P-125 digitale piano + draagtas', 'De P-125 is Yamaha''s opvolger voor de succesvolle P-115, maar nu met nog meer functies en dus mogelijkheden. In de piano vind je 24 standaardklanken, waaronder de samples van een Yamaha CF III-concertvleugel. Daarnaast kun je genieten van wel 20 bas- en drumtracks, uiteenlopend van stevige rock tot ballads en swingende Latin. Zo heb je gelijk een sfeervolle begeleiding bij je spel. Voor het spelen zelf maak je gebruik van het klavier met 88 Graded Hammer Standard-toetsen. Hierbij heb je toetsaanslag, vergelijkbaar met een vleugel, inclusief zwaardere aanslag in het laag en lichtere aanslag in het hoog. Dat alles is verwerkt in een compacte, stijlvolle behuizing, met overzichtelijke bediening. Deze piano is uitgevoerd in het zwart. Het betreft hier alleen de piano, een bijpassend onderstel en tri-pedaal zijn los verkrijgbaar.', '100435', 849.99, 969.99, '16 kg',1);
+  
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht, productgroep_id) values ('Ibanez Guitar', 'Ibanez brengt met de GRX40 uit de Gio Series een elektrische gitaar die zich uitstekend leent voor beginners die het instrument willen ontdekken. Dit model heeft namelijk een zogeheten HSS-elementconfiguratie. Dit houdt in dat er zowel een humbucker en twee single-coil elementen in de populieren body zitten. Hierdoor krijg je een breed scala aan mogelijke klanken tot je beschikking; van stevige rock tot en met warme cleantonen. Daarnaast kun je de tone- en volumeregelaars gebruiken voor extra nuance en kun je met de tremolo allerlei vibrato-effecten toevoegen.', '077030122-3', 150, 160, '3.9 kg',1);
+  
 insert into products (name, description, artikelcode, price, adviesprijs, gewicht, productgroep_id) values ('Fender Guitar', 'Ibanez Fender Guitar, elektrisch', '445924201-X', 350, 400, '15 kg',1);
+  
 insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Devine PRO Koptelefoon', 'Bluetooth headphone', '693155505-7', 40, 60, '1.1');
+  
 insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Yamaha Stage shellset Pure White Drum', 'Volledig drumstel', '686928463-6', 780, 900, '50 kg');
 
 insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Fazley elektrische basgitaar rood', 'Basgitaar Rood', '492662523-7', 80, 110, '5 kg');
@@ -69,22 +73,26 @@ insert into products (name, description, artikelcode, price, adviesprijs, gewich
 
 insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Pioneer DDJ-400 + controller flightbag', 'De Pioneer DDJ-400 is al snel uitgegroeid tot één van de meest populaire controllers van dit moment. Het is de ideale tweekanaals controller om te leren draaien met zijn tutorial-functie. Hij was al compact en handzaam maar nu wordt hij geleverd inclusief de DJC-B/WEGO3+BAG. Dat is een zeer handige en stevige controller flightbag, speciaal ontworpen om passend te zijn voor de DDJ-400. Hij heeft een schouderband, waardoor het makkelijk is je controller van A naar B te vervoeren. Tevens beschermt hij de controller tegen stootschade en stof.', '845987458', 349, 360, '4.2 kg');
 
-insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('
-Fenton SBS60 Bluetooth-speaker met LED-verlichting', 'De Fenton SBS60 is een 50W actieve fullrange 2 x 4 inch speaker. Zijn kleurige LED-strips, die om de grill van de speakers heen lopen, lichten op met de muziek die je er op afspeelt. Hij heeft een Aux- en een microfoon-ingang. Maar je kunt ook direct MP3', '492662523-7', 80, 110, '10 kg');
+insert into products (name, description, artikelcode, price, adviesprijs, gewicht) values ('Fenton SBS60 Bluetooth-speaker met LED-verlichting', 'De Fenton SBS60 is een 50W actieve fullrange 2 x 4 inch speaker. Zijn kleurige LED-strips, die om de grill van de speakers heen lopen, lichten op met de muziek die je er op afspeelt. Hij heeft een Aux- en een microfoon-ingang. Maar je kunt ook direct MP3', '4925485', 80, 110, '10 kg');
 
 insert into winkels (winkelcode,name) values ('ROT', 'Rotterdam');
 insert into winkels (winkelcode,name) values ('UTR','Utrecht');
 insert into winkels (winkelcode,name) values ('AMS', 'Amsterdam');
-insert into winkels (winkelcode,name) values ('GR', 'Groningen');
 insert into winkels (winkelcode,name) values ('LIM','Limburg');
 
 insert into artikel_winkel (product_id, winkel_id) values (1,1);
 insert into artikel_winkel (product_id, winkel_id) values (1,2);
+insert into artikel_winkel (product_id, winkel_id) values (1,3);
+insert into artikel_winkel (product_id, winkel_id) values (1,4);
 insert into artikel_winkel (product_id, winkel_id) values (2,1);
-insert into artikel_winkel (product_id, winkel_id) values (3,3);
+insert into artikel_winkel (product_id, winkel_id) values (2,2);
+insert into artikel_winkel (product_id, winkel_id) values (2,4);
+insert into artikel_winkel (product_id, winkel_id) values (3,2);
+insert into artikel_winkel (product_id, winkel_id) values (3,1);
 
   
-insert into voorraad (artikel_winkel_id ,name) values (1,'ja');
-insert into voorraad (artikel_winkel_id ,name) values (2,'nee');
-insert into voorraad (artikel_winkel_id ,name) values (3,'binnenkort');
-insert into voorraad (artikel_winkel_id ,name) values (4,'binnenkort');
+insert into voorraad (artikel_winkel_id ,name) values (1,'Ja');
+insert into voorraad (artikel_winkel_id ,name) values (2,'Nee');
+insert into voorraad (artikel_winkel_id ,name) values (3,'Laatste items');
+insert into voorraad (artikel_winkel_id ,name) values (4,'Binnenkort');
+insert into voorraad (artikel_winkel_id ,name) values (5,'Laatste items');
